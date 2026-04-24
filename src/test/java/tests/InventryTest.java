0ba2c80e4eb62f71a86c12c61abd0100fb9c5d96
sangamesh <sangamesh@sangameshs-MacBook-Air.local>
2026-04-24 15:20:26 +0530

    Added new Inventry test

---
+++ src/test/java/tests/InventryTest.java
@@ -0,0 +1,54 @@
+package tests;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.Assert;
+import org.testng.AssertJUnit;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import pages.InventoryPage;
+import pages.LoginPage;
+
+public class InventryTest extends BaseTest
+{
+	
+	LoginPage loginpage;
+	InventoryPage inventrypage;
+	
+	@BeforeMethod
+	public void setUp()
+	{
+		loginpage=new LoginPage(driver);
+	    inventrypage=new InventoryPage(driver);
+		loginpage.login("standard_user","secret_sauce");
+	}
+	
+	@Test
+	public void testPresenceOfHamBurgerButton()
+	{
+		
+		Assert.assertTrue(inventrypage.presenceOfHamBurgerButton(), "HamBurger Menu is not present");
+	}
+	@Test
+	public void testIsHambergerMenuOpen()
+	{
+		
+		Assert.assertTrue(inventrypage.isHambergerMenuOpen(), "HamBurger Menu is not Open");
+	}
+	@Test
+	public void testClickOnAllItemsLink()
+	{
+		inventrypage.openHambergerMenu();
+		inventrypage.clickOnAllItemsLink();
+	}
+	@Test
+	public void testAboutPageredirect()
+	{
+		inventrypage.openHambergerMenu();
+		inventrypage.clickAllAboutLink();
+	}
+	
+
+}
